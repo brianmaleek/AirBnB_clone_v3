64e99e05dad2c1caa4ffcb0f873fb4a0cb61b508
brianmaleek <maleekmalik@gmail.com>
2023-10-30 18:05:52 +0300

    app.py flask app

---
+++ api/v1/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+module for flask REST App
+"""
+from flask import Flask, jsonify, make_response
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+
+""" Register the app_views blueprint to the Flask Instance """
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ close the storage when the app context is torn down"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 error handler """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    """ Run the app """
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True, debug=True)
