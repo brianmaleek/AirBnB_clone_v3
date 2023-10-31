8ff8923dd11c620228901380e1390614c41fb52b
brianmaleek <maleekmalik@gmail.com>
2023-10-31 13:56:28 +0300

    security improvements to store passwords in MD5

--- models/base_model.py
+++ models/base_model.py
@@ -58,13 +58,15 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, save_to_disk=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        if not save_to_disk:
+            new_dict.pop("password", None)
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
