ee7c9afb5ad3eea12d7edf7dd3dda13f7e29c2cb
brianmaleek <maleekmalik@gmail.com>
2023-11-01 03:14:57 +0300

    endpoint fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,12 +4,6 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
 
 
 @app_views.route("/status", strict_slashes=False, methods=["GET"])
@@ -22,11 +16,11 @@ def get_status():
 def get_stats():
     """ retrieves the number of each objects by type """
     stats = {
-        "states": storage.count(State),
-        "cities": storage.count(City),
-        "amenities": storage.count(Amenity),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "users": storage.count(User)
+        "states": storage.count("State"),
+        "cities": storage.count("City"),
+        "amenities": storage.count("Amenity"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "users": storage.count("User")
     }
     return jsonify(stats)
